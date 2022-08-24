1d4a06a93f22af4b7992dfbde0f137bef5c0d964
Asther Marie Moreno <ashterfuentes@gmail.com>
2022-08-24 18:08:19 -0500

    Update .gitignore

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // active state
-const ratings = document.querySelectorAll('.rating-circle');
+const ratings = document.querySelectorAll(".rating-circle");
 let selectedRating;
 ratings.forEach((item, i) => {
-    item.addEventListener('click', (event) => {
-        //handle click
-        ratings.forEach((item) => {
-            item.classList.remove('orange-background');
-        });
-
-        item.classList.add('orange-background');
-        console.log(item.innerText);
-        selectedRating = item.innerText;
+  item.addEventListener("click", (event) => {
+    //handle click
+    ratings.forEach((item) => {
+      item.classList.remove("orange-background");
     });
+
+    item.classList.add("orange-background");
+    console.log(item.innerText);
+    selectedRating = item.innerText;
+  });
 });
 
 // hides the initial view ratings on submit
-const submitButton = document.querySelector('.submit');
-const initialView = document.querySelector('.initial-view');
-const submittedView = document.querySelector('.submitted-view');
-const spanRating = document.querySelector('.rating');
+const submitButton = document.querySelector(".submit");
+const initialView = document.querySelector(".initial-view");
+const submittedView = document.querySelector(".submitted-view");
+const spanRating = document.querySelector(".rating");
 
 // will show the submitted rating and thank you message
-submitButton.addEventListener('click', (event) => {
-    ratings.forEach((rating) => {
-        if (rating.classList.contains('orange-background')) {
-            initialView.classList.add('hidden');
-            submittedView.classList.remove('hidden');
-            spanRating.innerText = selectedRating;
-        }
-    });
+submitButton.addEventListener("click", (event) => {
+  ratings.forEach((rating) => {
+    if (rating.classList.contains("orange-background")) {
+      initialView.classList.add("hidden");
+      submittedView.classList.remove("hidden");
+      spanRating.innerText = selectedRating;
+    }
+  });
 });
